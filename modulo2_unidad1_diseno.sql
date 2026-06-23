a42ae9fe72a37d7506327e7b718fe8685d8bf290
zalazarjcc <zalazarjcc@gmail.com>
2026-06-22 21:24:18 -0300

    Update modulo2_unidad1_diseno.sql

--- modulo2_unidad1_diseno.sql
+++ modulo2_unidad1_diseno.sql
@@ -1,13 +1,13 @@
 CREATE TABLE CLIENTES (
-ID_CLIENTES INT,
-NOMBRE VARCHAR(100),
-PERFIL_BIO VARCHAR(MAX),
-FECHA_REGISTRO DATE
+id_clientes INT, -- se usa entero porque identificamos un valor numerico para idcentificar clientes
+nombre VARCHAR(100),-- los nombre son textos
+perfil_bio TEXT, -- aca vamos a poner una nota asi que necesitamos texto largo
+fecha_registro DATE -- porque solo necesitamos la fecha
 );
 
 CREATE TABLE PRODUCTOS (
-ID_PRODUCTO INT,
-DESCRIPCION VARCHAR(255),
-PRECIO DECIMAL(10,2),
-ESTA_ACTIVO INT -- SE TOMA ENTERO PARA DEFINIR 0 SI ESTA INACTIVO Y 1 SI ES ACTIVO
+id_producto INT,-- el id sera numerico
+descripcion VARCHAR(255), -- cuenta que es el producto
+precio DECIMAL(10,2),-- es un numero de 10 digitos y 2 decimales
+esta_activo INT -- SE TOMA ENTERO PARA DEFINIR 0 SI ESTA INACTIVO Y 1 SI ES ACTIVO
 );
